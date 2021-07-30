3abbe71b7e559a8018c39084e21ac0f0fee745b7
wahabmangat <abdulwahabmangat@gmail.com>
2021-07-30 18:15:40 +0500

    Add remaining testcases in domino problem.

--- problem2.rb
+++ problem2.rb
@@ -1,6 +1,7 @@
 def domino(s)
 domino_arr= []
 domino_arr = s.split(",")
+puts "---"
 puts "Domino: "
 print domino_arr
 domino_len = 1
@@ -11,4 +12,8 @@ for i in -1..(domino_arr.length - 2) do
 end
 return domino_len
 end
- puts "\nNumber of tiles in longest sequence = #{domino("1-1,3-5,5-5,5-4,4-2,1-3")}"
+puts "\nNumber of tiles in longest sequence = #{domino("1-1,3-5,5-2,2-3,2-4")}"
+puts "\nNumber of tiles in longest sequence = #{domino("3-2,2-1,1-4,4-4,5-4,4-2,2-1")}"
+puts "\nNumber of tiles in longest sequence = #{domino("5-5,5-5,4-4,5-5,5-5,5-5,5-5,5-5,5-5,5-5")}"
+puts "\nNumber of tiles in longest sequence = #{domino("1-1,3-5,5-5,5-4,4-2,1-3")}"
+puts "\nNumber of tiles in longest sequence = #{domino("1-2,2-2,3-3,3-4,4-5,1-1,1-2")}"
